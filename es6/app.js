5f5ec8f4e3cd0a6f1de57725c5911a325a618472
Mehedi-Hasan-code <quazimehedihasanovi@gmail.com>
2025-12-06 20:07:53 +0600

    learned package.json in details

--- es6/app.js
+++
@@ -1,2 +0,0 @@
-const { a, name, bool } = require('./math');
-console.log(a, name, bool);
--- es6/math.js
+++
@@ -1,7 +0,0 @@
-const a = 10;
-const name = 'anurag';
-const bool = true;
-exports.a = a;
-exports.name = name;
-exports.bool = bool;
-

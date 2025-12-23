ef4f991b230596a26f05174519ebfc5bd09cbb06
Mehedi-Hasan-code <quazimehedihasanovi@gmail.com>
2025-12-23 19:25:40 +0600

    b

--- es6/buffer.js
+++ es6/buffer.js
@@ -1,18 +1,35 @@
-import { Buffer } from "node:buffer";
+import { Buffer, constants } from "node:buffer";
 
-const nodeBuffer = Buffer.alloc(4)
-const a = new ArrayBuffer(4)
-const nodeBuffer2 = Buffer.from(a)
-const uint8Array = new Uint8Array(a)
+// const nodeBuffer1 = Buffer.alloc(50000);
+// const nodeBuffer2 = Buffer.allocUnsafe(5000)
 
-uint8Array[0] = 0x43
-uint8Array[1] = 0x6f
-uint8Array[2] = 0x72
-uint8Array[3] = 0x50
+// // console.log(nodeBuffer1);
+// // console.log(nodeBuffer2);
 
-console.log(nodeBuffer);
-console.log(nodeBuffer2);
+// console.log(nodeBuffer1.toString())
+// console.log(nodeBuffer2.toString('utf-8'));
 
-console.log(nodeBuffer2.buffer === uint8Array.buffer);
+// const a = Buffer.alloc(4)
+// const z = Buffer.alloc(4)
 
-console.log(nodeBuffer2.toString('utf-8'));
+// const joined = Buffer.concat([a,z])
+
+// console.log(joined);
+// const b = Buffer.allocUnsafe(4095);
+// const c = Buffer.allocUnsafe(4095 - 7)
+
+// b[4095] = 1
+// c[0] = 2
+
+// console.log(b.buffer === c.buffer);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+const b = Buffer.from('a'.repeat(constants.MAX_STRING_LENGTH))
+const d = b.toString()
+
+console.log(d);
+
